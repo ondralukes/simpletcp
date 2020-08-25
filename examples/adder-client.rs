ae457bbcbf6447fe033ff415fdcda83a482e9f20
ondralukes <mail@ondralukes.cz>
2020-08-25 19:49:58 +0200

    Added example

---
+++ examples/adder-client.rs
@@ -0,0 +1,33 @@
+use simpletcp::simpletcp::{TcpStream, Message};
+use rand::prelude::StdRng;
+use rand::{SeedableRng, RngCore};
+use std::convert::TryInto;
+use std::time::Instant;
+
+fn main(){
+    println!("Connecting");
+    let mut conn = TcpStream::connect("127.0.0.1:4328").unwrap();
+    conn.wait_until_ready().unwrap();
+    println!("Connection ready");
+
+    let mut rand = StdRng::from_entropy();
+    loop {
+        let mut rand_buffer = [8;8];
+        rand.fill_bytes(&mut rand_buffer[0..8]);
+
+        rand_buffer[3] = 0;
+        rand_buffer[7] = 0;
+
+        let a = i32::from_le_bytes(rand_buffer[0..4].try_into().unwrap());
+        let b = i32::from_le_bytes(rand_buffer[4..8].try_into().unwrap());
+        print!("{:010} + {:010} = ", a, b);
+        let mut message = Message::new();
+        message.write_i32(a);
+        message.write_i32(b);
+        let time = Instant::now();
+        conn.write(&message).unwrap();
+
+        let mut response = conn.read_blocking().unwrap();
+        println!("{:010} [{} us]", response.read_i32().unwrap(), time.elapsed().as_micros());
+    }
+}
---
+++ examples/adder-server.rs
@@ -0,0 +1,72 @@
+use simpletcp::simpletcp::{TcpServer, Message, Error};
+
+fn main(){
+    let server = TcpServer::new("0.0.0.0:4328").unwrap();
+
+    let mut clients = Vec::new();
+    loop {
+        // Check for new clients
+        match server.accept().unwrap() {
+            None => {},
+            Some(client) => {
+                clients.push(Some(client));
+            },
+        }
+
+        // Handle clients
+        for client_opt in &mut clients{
+            let client = client_opt.as_mut().unwrap();
+            match client.read(){
+                Ok(msg) => {
+                    match msg {
+                        None => {},
+                        Some(mut msg) => {
+                            let a = msg.read_i32().unwrap();
+                            let b = msg.read_i32().unwrap();
+
+                            let r = a+b;
+
+                            let mut response = Message::new();
+                            response.write_i32(r);
+                            client.write(&response).unwrap();
+                        },
+                    }
+                },
+                Err(err) => {
+                    match err {
+                        Error::NotReady => {
+                            match client.get_ready(){
+                                Ok(ready) => {
+                                    if ready {
+                                        println!("Client became ready!");
+                                    }
+                                },
+                                Err(_) => {
+                                    println!("Error while getting ready");
+                                },
+                            }
+                        },
+                        Error::EncryptionError(_) => {
+                            println!("Error::EncryptionError");
+                        },
+                        Error::TcpError(_) => {
+                            println!("Error::TcpError");
+                        },
+                        Error::ConnectionClosed => {
+                            println!("Error::ConnectionClosed");
+                            client_opt.take();
+                        },
+                    }
+                },
+            }
+        }
+
+        //Remove closed client
+        clients.retain(|t|{
+            if t.is_none(){
+                println!("Removed client");
+            }
+            t.is_some()
+        });
+    }
+}
