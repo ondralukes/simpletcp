8ab3940935663c961afd6377aa96fb6589bd934f
ondralukes <mail@ondralukes.cz>
2020-09-01 11:53:52 +0200

    Windows poll_set

--- src/utils/platform/unix.c
+++ src/utils/platform/unix.c
@@ -1,12 +1,13 @@
 #include "interface.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <poll.h>
 
 const short ev_pollin = POLLIN;
 const short ev_pollout = POLLOUT;
 
 int c_poll(Fd* fds, unsigned int length, short events, int timeout){
-    struct pollfd p[length];
+    struct pollfd* p = malloc(sizeof(struct pollfd) * length);
     for(unsigned int i = 0;i<length;i++){
         p[i].fd = fds[i];
         p[i].events = events;
@@ -16,8 +17,10 @@ int c_poll(Fd* fds, unsigned int length, short events, int timeout){
 
     for(unsigned int i = 0;i<length;i++){
         if(p[i].revents == events){
+            free(p);
             return i;
         }
     }
+    free(p);
     return -1;
 }
--- src/utils/platform/windows.c
+++ src/utils/platform/windows.c
@@ -5,11 +5,21 @@
 const short ev_pollin = POLLRDNORM;
 const short ev_pollout = POLLWRNORM;
 
-short c_poll(Fd fd, short events, int timeout){
-    struct pollfd p;
-    p.fd = fd;
-    p.events = events;
+int c_poll(Fd* fds, unsigned int length, short events, int timeout){
+    struct pollfd* p = malloc(sizeof(struct pollfd) * length);
+    for(unsigned int i = 0;i<length;i++){
+        p[i].fd = fds[i];
+        p[i].events = events;
+    }
 
-    WSAPoll(&p, 1, timeout);
-    return p.revents;
+    WSAPoll(p, length, timeout);
+
+    for(unsigned int i = 0;i<length;i++){
+        if(p[i].revents == events){
+            free(p);
+            return i;
+        }
+    }
+    free(p);
+    return -1;
 }
